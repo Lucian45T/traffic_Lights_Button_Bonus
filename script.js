ac9076ebfd0ac474ea8e464bb075f72e246a7434
Lucian45T <155108954+Lucian45T@users.noreply.github.com>
2025-06-17 22:49:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-setInterval(function Change() {
-    change(document.getElementById('colour'));}, 10000);
+setInterval(function ChangeTenSecconds() {
+    change();}, 10000);
+
+let button = document.getElementById("colour");
+let colors = ["red", "yellow", "green"];
+let written = ["red", "yellow", "green"];
+let index = 0;
+let index2 = 0;
+ 
 function change() {
-    let button =  document.getElementById('colour');
-    if (button.classList.contains("btn-danger")) {
-      button.classList.remove("btn-danger");
-      button.classList.add("btn-warning");
-      document.getElementById('colour').innerHTML = "Yellow"
-    }else if (button.classList.contains("btn-warning")) {
-      button.classList.remove("btn-warning");
-      button.classList.add("btn-success");
-      document.getElementById('colour').innerHTML = "Green"
-    }else if (button.classList.contains("btn-success")) {
-    button.classList.remove("btn-success");
-    button.classList.add("btn-danger");
-    document.getElementById('colour').innerHTML = "Red"
-    }
+   let color = colors[index];
+   let write = written[index2]; 
+   button.style.backgroundColor = color;
+   document.getElementById('colour').textContent = write;
+   index = index >= colors.length - 1 ? 0 : index + 1;
+   index2 = index2 >= written.length - 1 ? 0 : index2 + 1;
 }
