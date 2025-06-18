e7b542e9c92d58e3a2597a3fb6a09a46be816e13
Lucian45T <155108954+Lucian45T@users.noreply.github.com>
2025-06-18 22:10:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 setInterval(function ChangeTenSecconds() {
-    change();}, 10000);
+change();}, 10000);
 
 let button = document.getElementById("colour");
 let colors = ["red", "yellow", "green"];
-let written = ["red", "yellow", "green"];
 let index = 0;
-let index2 = 0;
  
 function change() {
-   let color = colors[index];
-   let write = written[index2]; 
+   let color = colors[index]; 
    button.style.backgroundColor = color;
-   document.getElementById('colour').textContent = write;
-   index = index >= colors.length - 1 ? 0 : index + 1;
-   index2 = index2 >= written.length - 1 ? 0 : index2 + 1;
+   document.getElementById('colour').textContent = color;
+   ++index;
+   if (index == colors.length) {
+       index = 0;
+   }
 }
