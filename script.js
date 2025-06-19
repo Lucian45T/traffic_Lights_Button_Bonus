8fcc0725470f0af99866db58263e454de9b3e540
Lucian45T <155108954+Lucian45T@users.noreply.github.com>
2025-06-19 22:24:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 setInterval(function ChangeTenSecconds() {
-change();}, 10000);
+   change();}, 10000);
 
 let button = document.getElementById("colour");
 let colors = ["red", "yellow", "green"];
 let index = 0;
  
 function change() {
-   let color = colors[index]; 
+   let color = colors[index % 3]; 
    button.style.backgroundColor = color;
    document.getElementById('colour').textContent = color;
    ++index;
-   if (index == colors.length) {
-       index = 0;
-   }
 }
